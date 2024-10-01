aa628acbfafa70fd79624d27f3f2b2005af3db1c
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2024-10-01 16:25:21 +0100

    chore: Remove Blog component from Home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,12 +3,10 @@ import HeroArea from "@/components/HeroArea";
 import Features from "@/components/Features";
 import About from "@/components/About";
 import WorkProcess from "@/components/WorkProcess";
-import Pricing from "@/components/Pricing";
 import Screens from "@/components/Screens";
 import Cta from "@/components/Cta";
 import Testimonials from "@/components/Testimonials";
 import Faq from "@/components/Faq";
-import Blog from "@/components/Blog";
 import Clients from "@/components/Clients";
 import Contact from "@/components/Contact";
 
@@ -28,7 +26,6 @@ export default function Home() {
       <Cta />
       <Testimonials />
       <Faq />
-      <Blog />
       <Clients />
       <Contact />
     </>
